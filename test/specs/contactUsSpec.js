ddaa458a1d9eeba4c4d00bb112b8279f635a5aa3
Alistair Kung <alistaircckung@gmail.com>
2017-10-15 21:59:51 +0100

    updated selenium server and geckodriver, passed first two test cases

--- test/specs/contactUsSpec.js
+++ test/specs/contactUsSpec.js
@@ -1,1 +1,20 @@
 var assert = require('assert');
+
+describe('contact us', function() {
+    it('should be accessable from a link from the Navbar', function () {
+        browser.url('http://www.qaworks.com/');
+        browser.click('#menu-item-18894')
+        var title = browser.getTitle();
+        assert.equal(title, 'CONTACT US | QAWorks Limited');
+    });
+    it('should submit a contact form if all fields are entered correctly', function () {
+        browser.url('http://www.qaworks.com/contact-us/');
+        browser.setValue('input[name="your-name"]', 'j.Bloggs');
+        browser.setValue('input[name="your-email"]', 'j.Bloggs@qaworks.com');
+        browser.setValue('input[name="your-company"]', 'test automation');
+        browser.setValue('textarea[name="your-message"]', 'please contact me I want to find out more');
+        browser.submitForm('#contact-us-send');
+        browser.waitForVisible('.wpcf7-response-output', 5000);
+        assert.equal(browser.getText('.wpcf7-response-output'), '×\nTHANK YOU FOR YOUR MESSAGE.');
+    });
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-    
+
     //
     // ==================
     // Specify Test Files
@@ -72,7 +72,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://www.qaworks.com/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
